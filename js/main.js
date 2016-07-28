d0890081322ff762b254f1a3befcbd02291cd087
Will Moody <williamcmoody@gmail.com>
2016-07-28 14:58:51 -0700

    starting lesson 14

--- js/main.js
+++ js/main.js
@@ -1,24 +1,64 @@
-const counter = (state = 0, action) => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const todo = (state, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== state.id) {
+      return state;
+      }
+
+      return  Object.assign({}, state, {
+      completed: !state.completed
+      })
     default:
       return state;
   }
 }
 
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return  [
+          ...state,
+          todo(null, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  };
+};
+
+
+const TodoList = ({state, onAddTodo}) => (
+  <div>
+    <h1>Hey</h1>
+    <button onClick={onAddTodo}></button>
+  </div>
+);
+
+
+
 import { createStore } from 'redux';
-const store = createStore(counter);
+const store = createStore(todos);
 
 const render = () => {
-  document.body.innerText = store.getState();
-}
+  ReactDOM.render(
+    <TodoList
+      state={store.getState()}
+      onAddTodo={() => store.dispatch({type: 'ADD_TODO'})}
+      />,
+    document.getElementById('root')
+  )
+};
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT'});
-});
